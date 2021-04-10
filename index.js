ad47bd0a2fe8d02870e4df333790dec1b0d7f933
shaunvan119 <shaunvan@live.com.au>
2021-04-10 10:46:07 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+    // pass in the API URL
+   return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json()) // returning the URL to the JSON
+  .then(json => renderBooks(json)); // Chain method to avoke the function then into console log
   
-}
-
-function renderBooks(books) {
+  
+ }
+  
+ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+ }
+  
+ document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+ });
+ 
